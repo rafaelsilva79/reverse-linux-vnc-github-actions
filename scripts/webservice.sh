5f1f8effc2def1b38832d42ec7899f006e540a91
Rui Pinheiro <136269190+ruipinheiro99@users.noreply.github.com>
2025-08-09 19:10:43 +0100

    Update webservice.sh

--- scripts/webservice.sh
+++ scripts/webservice.sh
@@ -14,12 +14,39 @@ then
     # Setup cloud9
     cd ~
     mkdir workspace
-    sudo apt-get install python2
-    sudo apt-get install nodejs
+	# Python 2.7
+    mkdir python2
+    cd python2
+    sudo apt-get install libssl-dev
+    wget https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz
+    tar xzf Python-2.7.18.tgz
+    cd Python-2.7.18
+    sudo ./configure --enable-optimizations
+    sudo make altinstall
+    sudo ln -s /usr/local/bin/python2.7 /usr/bin/python2
+    sudo rm /usr/bin/python
+    sudo ln -s /usr/local/bin/python2.7 /usr/bin/python
+    cd ..
+    wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
+    python2 get-pip.py
+    pip2 install requests==2.12.4
+    cd ~
+	# Node.js 6
+    wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+    source ~/.bashrc
+    nvm install 6
+    nvm use 6
+    nvm alias default 6
+	# Cloud9
     git clone https://github.com/c9/core.git c9sdk
     cd c9sdk
-    ./scripts/install-sdk.sh
-    nodejs ./server.js -p 8080 -l 0.0.0.0 -a linux:$VNC_PASSWORD -w ../workspace &
+    sudo apt-get install tmux
+    echo 'if [ -f ~/.bashrc ]; then source ~/.bashrc; fi' >> ~/.bash_profile
+    wget https://raw.githubusercontent.com/c9/install/refs/heads/master/link.sh -O scripts/link.sh
+    chmod +x scripts/link.sh
+    ./scripts/link.sh
+    npm install
+    node ./server.js -p 8080 -l 0.0.0.0 -a linux:$VNC_PASSWORD -w ../workspace &
 
 elif [ "$RUNNER_WEBSERVICE" == "vscode" ]
 then
